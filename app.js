94bfccebcefaaaac032cf5125dc560906ea008b8
unknown <wuraolaayinke12@.com>
2022-12-29 11:42:18 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// function clickhere() {
+//     var a = 2;
+//     var b = 1;
+//     var sum = a + b;
+//     if (sum < 2 || sum == 4){
+//         alert("Nice One");
+//     } else if (sum == 3 || sum < 3){
+//         alert("Testing");
+//     }}
+function clickhere(){
+    var sum= "see-more";
+    alert(sum+"here");
+}
+
+// const d= new Date()
+
+// document.getElementById("birth").innerHTML=d
